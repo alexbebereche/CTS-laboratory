88818847d18e31bcd3cbb714b363849ebe83ab9c
Alex Bebereche <alexbebereche99@yahoo.com>
2021-03-16 11:30:42 +0200

    correct formula

--- Seminar3 - Clean Code Refactoring/src/ro/ase/cts/g1092/refactor/phase5/services/Marketing2021Service.java
+++ Seminar3 - Clean Code Refactoring/src/ro/ase/cts/g1092/refactor/phase5/services/Marketing2021Service.java
@@ -6,7 +6,7 @@ public class Marketing2021Service implements MarketingStrategyInterface{
 
 	@Override
 	public float getFidelityDiscount(int yearsSinceRegistration) {
-		return (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : 0;
+		return (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : (float)yearsSinceRegistration/100;
 	}
 
 	
