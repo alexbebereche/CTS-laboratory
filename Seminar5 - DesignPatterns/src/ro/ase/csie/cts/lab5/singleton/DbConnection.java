4b63ef3541d771d9b502ecfbb21caa67db1da41f
Alex Bebereche <alexbebereche99@yahoo.com>
2021-03-23 10:29:28 +0200

    singleton implementation commit before break

---
+++ Seminar5 - DesignPatterns/src/ro/ase/csie/cts/lab5/singleton/DbConnection.java
@@ -0,0 +1,51 @@
+package ro.ase.csie.cts.lab5.singleton;
+
+public class DbConnection {
+	String socket;
+	String schema;
+	
+	// version 1 - eager approach; dont want for anyone to ask for it,create when its loaded
+//	private static DbConnection dbConnection = new DbConnection();
+	
+	//version 2 - lazy approach; wait for everyone to ask for it
+	private static DbConnection dbConnection = null;
+	
+	// static class constructor
+	// class static initializer
+	// uses the class lazy-loading approach - the class will be loaded only when we need it
+	static {
+		//executed only once, when the class is loaded by the JVM
+		System.out.println("Loading database connection string");
+		
+	}
+	
+	//constructor initializer
+	{
+		//executed before each constructor call
+		System.out.println("Init connection string");
+	}
+	
+	private DbConnection() {
+		System.out.println("Creating a DB connection");
+		
+		// access a local settings file to get credentials and conn string
+		
+	}
+
+	private DbConnection(String socket, String schema) {
+		this.socket = socket;
+		this.schema = schema;
+	}
+	
+	//the method used to return the reference to the unique object
+	public static DbConnection getDbConnection() {
+		if(DbConnection.dbConnection == null) {
+			//you can get details from files, etc
+			dbConnection = new DbConnection();
+		}
+		
+		return DbConnection.dbConnection;
+	}
+	
+	
+}
