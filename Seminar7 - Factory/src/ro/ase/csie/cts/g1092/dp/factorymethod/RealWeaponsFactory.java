36f006e5fb53358b4f73a2a9d5def24743831cb3
Alex Bebereche <alexbebereche99@yahoo.com>
2021-04-06 11:04:39 +0300

    Factory method implementation

---
+++ Seminar7 - Factory/src/ro/ase/csie/cts/g1092/dp/factorymethod/RealWeaponsFactory.java
@@ -0,0 +1,31 @@
+package ro.ase.csie.cts.g1092.dp.factorymethod;
+
+import ro.ase.csie.cts.g1092.dp.simplefactory.AbstractWeapon;
+import ro.ase.csie.cts.g1092.dp.simplefactory.Bazooka;
+import ro.ase.csie.cts.g1092.dp.simplefactory.MachineGun;
+import ro.ase.csie.cts.g1092.dp.simplefactory.Pistol;
+import ro.ase.csie.cts.g1092.dp.simplefactory.WeaponType;
+
+public class RealWeaponsFactory extends AbstractWeaponsFactory{
+
+	@Override
+	public AbstractWeapon getWeapon(WeaponType type, String desc) {
+		AbstractWeapon weapon = null;
+		switch (type) {
+		case PISTOL: 
+			weapon = new Pistol(desc, 100);
+			break;
+		case MACHINE_GUN:
+			weapon = new MachineGun(desc, 500, 1000);
+			break;
+		case BAZOOKA:
+			weapon = new Bazooka(desc);
+			break;
+		default:
+			throw new UnsupportedOperationException("new type not covered");
+		}
+		
+		return weapon;
+	}
+
+}
